062af67429d44f57eb39f67366f743f0c5972c3d
Yosuke aoki <aoki-yosuke@fujitsu.com>
2025-03-25 11:07:25 +0900

    [青木] baseURLのプリフィックスをVITEに修正

--- src/apiClient.ts
+++ src/apiClient.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance } from 'axios';
 
 const apiClient: AxiosInstance = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL, // 環境変数から API のベース URL を取得
+  baseURL: process.env.VITE_API_BASE_URL, // 環境変数から API のベース URL を取得
   headers: {
     'Content-Type': 'application/json',
   },
